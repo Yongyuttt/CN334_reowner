b50057462b439183851f724f600417a2ffcb8067
Yongyuttt <pornpongsawadi.ko@gmail.com>
2024-04-07 17:04:12 +0700

    update

--- reowner/urls.py
+++ reowner/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from general import views as view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', view.home_view),
 
 ]
